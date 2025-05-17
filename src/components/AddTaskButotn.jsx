b24293d74bf132632f27794dc6e530d6dd750f19
shashwat2048 <shinghalshashwat@gmail.com>
2025-05-17 15:28:57 +0530

    feat: Enhance AddTaskButton with dynamic background color based on column length

--- src/components/AddTaskButotn.jsx
+++ src/components/AddTaskButotn.jsx
@@ -2,15 +2,19 @@
 import { LuCirclePlus } from "react-icons/lu";
 import { useState } from "react";
 import AddTaskModal from "../components/modals/AddTaskModal";
+import { useSelector } from "react-redux";
 
 export default function AddTaskButton(){
     const [isOpen, setIsOpen] = useState(false);
+    const cols = useSelector(store => store.app.columns);
+    
     return (
         <>
             {
                 isOpen && <AddTaskModal setIsOpen={setIsOpen} />
             }
-            <button className="flex flex-row items-center justify-center gap-2 bg-blue-300 p-2 rounded-2xl ml-16" onClick={() => setIsOpen(true)}>
+
+            <button className={`flex flex-row items-center justify-center gap-2 p-2 rounded-2xl ml-16 ${cols.length ? " bg-blue-400":"bg-blue-200 cursor-not-allowed"}`} disabled  onClick={() => setIsOpen(true)}>
             <LuCirclePlus /><p>Add new task</p>
             </button>
         </>
